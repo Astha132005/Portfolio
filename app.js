f175c0878573e2fcc2969bf9b9950345c6283791
ASTHA DAKHINRAY <155209614+Astha132005@users.noreply.github.com>
2025-07-15 20:26:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+const video1 = document.getElementById('projectVideo1');
+const video2 = document.getElementById('projectVideo2');
+const video3 = document.getElementById('projectVideo3');
+const video4 = document.getElementById('projectVideo4');
+const video5 = document.getElementById('projectVideo5');
+const video6 = document.getElementById('projectVideo6');
+
+// Sidebar elements //
+const sideBar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon')
+
+ 
+const hoverSign = document.querySelector('.hover-sign');
+
+const videoList =[video1, video2, video3, video4, video5, video6];
+
+videoList.forEach (function(video){
+    video.addEventListener("mouseover", function(){
+        video.play()
+        hoverSign.classList.add("active")
+    })
+    video.addEventListener("mouseout", function(){
+    video.pause();
+    hoverSign.classList.remove("active")
+})
+})
+
+// Sidebar elements //
+menu.addEventListener("click", function(){
+    sideBar.classList.remove("close-sidebar")
+    sideBar.classList.add("open-sidebar")
+});
+
+closeIcon.addEventListener("click", function(){
+    sideBar.classList.remove("open-sidebar");
+    sideBar.classList.add("close-sidebar");
+    
+})
+
+const cards = document.querySelectorAll('.carde');
+
+cards.forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = 'rotateX(0deg) rotateY(0deg) scale(1)';
+  });
+});
+
